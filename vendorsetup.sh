8932b5604295eecdff2d2252ffc248827c8d80b2
andrew.boren <andrew.boren@gmail.com>
2011-03-01 21:17:38 -0700

    vision: Remove non-evervolv build options.
    Change-Id: Id2e1fb5b36461e699df45a2d925795fa21299806

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo htc_vision-userdebug
-add_lunch_combo htc_vision-eng
+#We're only going to be building Evervolv, so comment this out for now
+#add_lunch_combo htc_vision-userdebug
+#add_lunch_combo htc_vision-eng
